0b07caf7ad06173b8fb8d601f8be1cbf6edef678
saharasil <58476824+saharasil@users.noreply.github.com>
2019-12-18 15:47:59 +0100

    mercredi javascript

---
+++ js/01_base_js.js
@@ -0,0 +1,17 @@
+/************SYNTAXE DE BASE **************/
+//1. Je vérifie toujours que ma page js et mon fichier html sont connectés:
+alert('ok js');
+
+//deux slashes pour un commentaire sur une ligne (ctrl +/)
+/*
+(shift + alt+a)
+ici un 
+commentaire sur
+plusiaurs lignes
+
+*/
+/*
+2 manière d'afficher une boite de dialogue:
+    => alert ('super, tu es arrivée sur mon site !')
+    =>prompt ('Quastion : on est quel jour ?'); avec la fonction prompt je dialogue dirctement avec l'utilisateur
+    */
