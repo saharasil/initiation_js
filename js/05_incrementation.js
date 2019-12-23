80fc14ab1f92bdbf6d5962ed4eb1ec1ba75a0269
saharasil <58476824+saharasil@users.noreply.github.com>
2019-12-23 16:53:56 +0100

    Update 05_incrementation.js

--- js/05_incrementation.js
+++ js/05_incrementation.js
@@ -2,21 +2,24 @@
 // /****************INcrémentation et décrémentation********************
 
 //Incrémentation
-var nb1 =1;
-nb1 =nb1 + 1;
-nb1+=1;
-//Ecriture simplifiée
-nb1++;
-console.log(nb1);
+// var nb1 =1;
+// nb1 =nb1 + 1;
+// nb1+=1;
+// //Ecriture simplifiée
+// nb1++;
+// console.log(nb1);
 
-//Décrémentation
-var nb2 = 1;
-nb2 = nb1 -1;
-nb2 -= 1;
-console.log(nb2);
- // Ecriture simplifiée
- nb2--;
- console.log(nb2);
+// //Décrémentation
+// var nb2 = 1;
+// nb2 = nb1 -1;
+// nb2 -= 1;
+// console.log(nb2);
+//  // Ecriture simplifiée
+//  nb2--;
+//  console.log(nb2);
 
  //subtilité
  nb1++; /* a le même résultat que */ ++nb1;
+ var nb1=5;
+ nb1= ++nb1; 
+ console.log(nb1);
