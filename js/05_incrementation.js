120c6933cdf3fefd66dd5374f1caf03b28f8e943
saharasil <58476824+saharasil@users.noreply.github.com>
2020-01-30 17:10:06 +0100

    fin de javascript

--- js/05_incrementation.js
+++ js/05_incrementation.js
@@ -6,8 +6,8 @@ var nb1 =1;
 nb1 =nb1 + 1;
 nb1+=1;
 //Ecriture simplifiée
-// nb1++;
-++nb1;
+nb1++;
+// ++nb1;
 console.log(nb1);
 
 // //Décrémentation
@@ -21,10 +21,10 @@ console.log(nb2);
 
  //subtilité
 //  nb1++; /* a le même résultat que */ ++nb1;
- var nb1 = 0;
- var nb2 = 5;
- y = nb1++;
- z = ++nb2;
+ var nb3 = 5;
+ var nb4 = 5;
+ var y = nb3++;
+ var z = ++nb4;
 //  z = nb2++;
  console.log(y);
  console.log(z);
