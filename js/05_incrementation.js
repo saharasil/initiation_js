da7bd2b727dac9b11434d25770a72d4000297363
saharasil <58476824+saharasil@users.noreply.github.com>
2020-01-30 06:35:33 +0100

    modification

--- js/05_incrementation.js
+++ js/05_incrementation.js
@@ -1,13 +1,13 @@
 // alert('js okay');    
 // /****************INcrémentation et décrémentation********************
 
-<<<<<<< HEAD
 // //Incrémentation
 var nb1 =1;
 nb1 =nb1 + 1;
 nb1+=1;
 //Ecriture simplifiée
-nb1++;
+// nb1++;
+++nb1;
 console.log(nb1);
 
 // //Décrémentation
@@ -25,29 +25,6 @@ console.log(nb2);
  var nb2 = 5;
  y = nb1++;
  z = ++nb2;
+//  z = nb2++;
  console.log(y);
  console.log(z);
-=======
-//Incrémentation
-// var nb1 =1;
-// nb1 =nb1 + 1;
-// nb1+=1;
-// //Ecriture simplifiée
-// nb1++;
-// console.log(nb1);
-
-// //Décrémentation
-// var nb2 = 1;
-// nb2 = nb1 -1;
-// nb2 -= 1;
-// console.log(nb2);
-//  // Ecriture simplifiée
-//  nb2--;
-//  console.log(nb2);
-
- //subtilité
- nb1++; /* a le même résultat que */ ++nb1;
- var nb1=5;
- nb1= ++nb1; 
- console.log(nb1);
->>>>>>> 80fc14ab1f92bdbf6d5962ed4eb1ec1ba75a0269
